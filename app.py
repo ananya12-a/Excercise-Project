b371dcf6a745a2936f8d3d4c7dbe17026a2c9474
ananya12-a <65915737+ananya12-a@users.noreply.github.com>
2020-07-13 21:53:05 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -39,35 +39,52 @@ jumbotron = dbc.Jumbotron(
 link_input = dbc.FormGroup(
     dbc.Container(
         [
-        dbc.Label("Enter a link and press submit",),
-        dbc.Input( type="text", bs_size="lg",className="form-control form-control-lg", placeholder="your link",id="inputLarge"),
-        dbc.FormText(""),
-        html.Button('Submit', id='submit-val',className='btn btn-info',type="submit"),
-        
-        dbc.Container(
-            dbc.Container(
-                dbc.Input(type="file", className="custom-file-input", id="inputGroupFile02"),
-                dbc.Label(className="custom-file-label", "Choose File")
-            ),
-            className="custom-file",
-            dbc.Container(
-                dbc.Span(
-                    "Upload"
+        dbc.Label("Enter a link and press submit", className= "col-form-label col-form-label-lg", ),
+        dbc.Row(
+            [
+                html.Div(
+                    [
+                        dbc.Input(placeholder="Your link", type="text", bs_size="lg"),
+                        dbc.FormText(""),
+                    ]
                 ),
-                className = "input-group-text",
-                id = "",
-            ),
-            className="input-group-append"
-        ),
-    ]
+                html.Div(
+                    [
+                        html.Button('Submit', id='submit-val',className='btn btn-info',type="submit"),
+                    ]
+                ),
+            ]
+        )
+        
+        ]
+    ),
+    id="link_input"
 )
 
+file_input = html.Div(
+    html.Div(
+        [
+            html.Div(
+                [dbc.Input(type="file", className = "custom-file-input", id = "inputGroupFile02"),
+                dbc.Label("Choose File", className="custom-file-label", html_for="inputGroupFile02")],
+                className = "custom-file"
+            ),
+            html.Span(
+                
+                    ["Upload",],
+                    
+                    id=""
+                ,className="input-group-text", 
+            ),
+        ]
+    )
+    ,className = "form-group",
+)
 
-#imagee = html.Div(
-#    html.Img(src=app.get_asset_url('img1.jpg'), style={'height':'10%', 'width':'45%','margin' : '3%'})
-#)
-app.layout = html.Div([dcc.Location(id="url"), jumbotron, link_input])
+images = html.Div(
+    html.Img(src=app.get_asset_url('img1.jpg'), style={'height':'10%', 'width':'45%','margin' : '3%'})
+)
+app.layout = html.Div([dcc.Location(id="url"), jumbotron, link_input,images,file_input])
 
 if __name__ == "__main__":
     app.run_server()
-ß
